7077a1255ffc0a0052b27d34e8f08b7a1afb6f41
onesio <onesiojr1@gmail.com>
2021-04-18 20:59:01 -0300

    colocando upload de imagem no post

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,7 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const db = require("../db");
-const produto = require("../produtoModel")
+const produto = require("../produtoModel");
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/images/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname))
+  }
+})
+
+const upload = multer({ storage: storage });
 
 /* GET users listing. */
 router.get('/', async function (req, res, next) {
@@ -11,16 +24,22 @@ router.get('/', async function (req, res, next) {
   res.send(produtos);
 });
 
-router.post('/', async function (req, res) {
-  const produtos = new produto(req.body);
-
+router.post('/', upload.single('img'), async function (req, res) {
   try {
-    await produtos.save();
-    res.send(produtos);
+    const prod = new produto({
+      img: `/images/${req.file.filename}`,
+      ...req.body,
+    });
+
+    const novoProduto = await prod.save();
+
+    res.send(novoProduto);
   } catch (error) {
+    console.log(error);
     res.status(500).send(error);
   }
 });
+
 
 router.put("/update/:id", async function (req, res, next) {
   try {
@@ -34,11 +53,11 @@ router.put("/update/:id", async function (req, res, next) {
 });
 
 
-router.delete("/delete/:id", async function (req, res) {
+router.delete("/delete/:id", async function (req, res, next) {
   try {
     await produto.findByIdAndDelete(req.params.id);
     res.send("Deletado com sucesso");
-  } catch {
+  } catch (error) {
     return next(error);
   }
 });
